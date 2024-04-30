682afce0d8258dfe2e2a4ccf50bbef4c24f2dfc4
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2024-04-30 17:24:43 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
-    <div className="bg-red-500 py-5 mx-6">
-    <h1>Hello tailwind</h1>
+    <div className="mx-6 bg-red-500 py-5">
+      <h1>hello</h1>
     </div>
   );
-}
+};
 export default Home;
